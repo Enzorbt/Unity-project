5ffc7c3839a74fe84592dc458517780d2b7ac860
loicLE-HENANF <loic.le-henanf@supinfo.com>
2024-01-11 23:10:45 +0100

    Added color and Created 2 test functions in CameraMovement

--- Project/Assets/Tests/PlayMode/CameraMovement.cs
+++ Project/Assets/Tests/PlayMode/CameraMovement.cs
@@ -4,23 +4,29 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class CameraMovement
+namespace Tests.PlayMode
 {
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator MoveRight()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
     
-    [UnityTest]
-    public IEnumerator MoveLeft()
+
+    public class CameraMovement
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MoveRight()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator MoveLeft()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
     }
+
 }
