12e83ad15969ab9054f3bdc18b01340e30311347
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-01-23 10:53:00 +0100

    Raycast New State
    - Separation of Script for Unit Collision
    - Advance Raycat

---
+++ Project/Assets/Unit/Scripts/UnitCollision/UnitRaycast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Supinfo.Project.Unit.Scripts.UnitCollision
+{
+    public class UnitRaycast : MonoBehaviour
+    {
+        private Unit unitScript;
+        private int rayLenght = 20;
+        private void Start()
+        {
+            unitScript = GetComponent<Unit>();
+        }
+        void Update()
+        {
+            // Get the direction of unit 
+            // Make the if else to catch the unit with a raycast 
+            // Clean Unity / componant 
+            // Comment 
+            Debug.DrawRay(transform.position, Vector3.left * rayLenght);
+            
+        }
+    }
+}
