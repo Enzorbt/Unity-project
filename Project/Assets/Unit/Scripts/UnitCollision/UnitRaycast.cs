69b8d52ed0f76fc34e4652bf99e4a67c3e91d314
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-02-10 16:12:57 +0100

    update collision systeme
    - Change Tag Allies / Enemy
    - Update Collision systeme
    - Can detect if it's an Enemy, or an Allies

--- Project/Assets/Unit/Scripts/UnitCollision/UnitRaycast.cs
+++ Project/Assets/Unit/Scripts/UnitCollision/UnitRaycast.cs
@@ -5,7 +5,8 @@ namespace Supinfo.Project.Unit.Scripts.UnitCollision
 {
     public class UnitRaycast : MonoBehaviour
     {
-        private float _rayLength = 2;
+        private float _rayMoovementLength = 2; // Need to be find in the unit data 
+        // private float _rayAttackLength = 1; // Need to be find in the unit data
         private IMovement _unitMovementScript;
         private RaycastHit2D _hit;
         private Vector3 _direction;
@@ -29,28 +30,45 @@ namespace Supinfo.Project.Unit.Scripts.UnitCollision
 
         private void FixedUpdate()
         {
-            PerformRaycast(_rayLength);
+            PerformRaycastMoovement(_rayMoovementLength);
+            //PerformRaycastAttack(_rayAttackLength);
         }
 
-        private void PerformRaycast(float length)
+        private void PerformRaycastMoovement(float length)
         {
             _hit = Physics2D.Raycast(transform.position, _direction, length, ~LayerMask.GetMask("Unit"));
 
             if (_hit.collider is not null)
             {
-                if (_hit.collider.gameObject != gameObject && _hit.collider.CompareTag("Unit"))
+                string[] hitTagParts = _hit.collider.tag.Split(',');
+                string[] myTagParts = gameObject.tag.Split(',');
+                if (hitTagParts[0] == "Unit")
                 {
-                    Debug.Log("Unit met: " + _hit.transform.name);
+                    if (hitTagParts[1] != myTagParts[1])
+                    {
+                        // Rencontre avec une unité enemy 
+                        //_unitMovementScript.StopMovement();
+                        Debug.Log("Enemy find !");
+                    }
                     _unitMovementScript.StopMovement();
                 }
             }
+            else
+            {
+                _unitMovementScript.StartMovement();
+            }
+        }
+
+        private void PerformRaycastAttack(float length)
+        {
+            // Vérfier si une unité et dans le range de l'unité pour l'attaquer (Avec le range d'attack récupèrer dans la data des unit)
         }
 
         private void OnDrawGizmos()
         {
             if (_unitMovementScript != null)
             {
-                Debug.DrawRay(transform.position, _direction * _rayLength, Color.green);
+                Debug.DrawRay(transform.position, _direction * _rayMoovementLength, Color.green);
             }
         }
     }
