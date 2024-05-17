7d66253bc66ec56b2efbe3a37778f083987b1bb3
loicLE-HENANF <loic.le-henanf@supinfo.com>
2024-05-17 18:57:29 +0200

    :sparkles: age and xp managment done (mostly) and changes to upgradeable

--- Project/Assets/Scripts/Interfaces/Upgrades/IUpgradable.cs
+++ Project/Assets/Scripts/Interfaces/Upgrades/IUpgradable.cs
@@ -1,14 +1,5 @@
 ﻿namespace Supinfo.Project.Scripts.Interfaces.Upgrades
 {
-    public enum UpgradeType
-    {
-        Attack,
-        Range,
-        GoldGiven,
-        Health,
-        Price
-    }
-    
     public interface IUpgradable
     {
         public void Upgrade(UpgradeType type); // upgrade a stat of the upgrade type (from enum)
---
+++ Project/Assets/Scripts/ScriptableObjects/Experience/ExperienceStatSo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Supinfo.Project.Scripts.ScriptableObjects.Experience
+{
+    [CreateAssetMenu(menuName = "MENUNAME", order = 0)]
+    public class ExperienceStatSo : ScriptableObject
+    {
+        [SerializeField] private List<float> experienceLevels;
+        public List<float> ExperienceLevel => experienceLevels;
+    }
+}
