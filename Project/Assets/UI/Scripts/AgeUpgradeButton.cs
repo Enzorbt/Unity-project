421cccc7a59aa4992779b1c613b98269e82b1e70
loicLE-HENANF <loic.le-henanf@supinfo.com>
2024-05-18 03:03:22 +0200

    :bug: fixed a bug with star and exp bar (now update and block at the last age) - changed scene

--- Project/Assets/UI/Scripts/AgeUpgradeButton.cs
+++ Project/Assets/UI/Scripts/AgeUpgradeButton.cs
@@ -1,4 +1,3 @@
-using System;
 using Supinfo.Project.Scripts.Events;
 using UnityEngine;
 using UnityEngine.UI;
@@ -23,17 +22,27 @@ namespace Supinfo.Project.UI.Scripts
         {
             image = transform.GetComponentInChildren<Image>();
             image.sprite = pressButtonImg;
+            EnableButton(false);
         }
 
 
         public void OnCanEvolve(Component sender, object data)
         {
             image.sprite = buttonImg;
+            EnableButton(true);
         }
         public void OnClick()
         {
-            
             onAgeUpgrade.Raise(this, 2);
+            image.sprite = pressButtonImg;
+            EnableButton(false);
+        }
+
+        public void EnableButton(bool value)
+        {
+            var button = transform.GetComponentInChildren<UnityEngine.UI.Button>();
+            if (button is null) return;
+            button.enabled = value;
         }
     }
 }
