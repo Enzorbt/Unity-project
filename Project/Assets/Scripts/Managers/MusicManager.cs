3a5de65c9946a23e3593d0b581ced6be26bc760c
Enzorbt <enzo.robert@supinfo.com>
2024-05-29 00:45:23 +0200

    :sparkles: Music (EasterEgg) added !

--- Project/Assets/Scripts/Managers/MusicManager.cs
+++ Project/Assets/Scripts/Managers/MusicManager.cs
@@ -14,20 +14,23 @@ namespace Supinfo.Project.Scripts.Managers
     {
         [FormerlySerializedAs("musicList")] 
         public MusicListSo musicListSo;
-        
+
         [SerializeField]
         private AudioSource audioSourceMusic;
-        
+
         [SerializeField]
         private AudioSource audioSourceSound;
 
         private int currentAge = 0;
+        private AudioClip originalMainMenuMusic;
+        private bool isEasterEggMusicPlaying = false;
 
         [SerializeField]
         private SceneName sceneName;
 
         private void Awake()
         {
+            originalMainMenuMusic = musicListSo.MenuMusic;
             PlayMusicForCurrentAge();
             audioSourceMusic.volume = PlayerPrefs.GetFloat("volume");
             audioSourceMusic.mute = PlayerPrefs.GetInt("isMuted") > 0;
@@ -54,14 +57,6 @@ namespace Supinfo.Project.Scripts.Managers
             }
             audioSourceMusic.Play();
         }
-        
-        public void onPlayButtonClickSound(Component sender, object data)
-        {
-            if (data is AudioClip buttonClip && audioSourceSound != null)
-            {
-                audioSourceSound.PlayOneShot(buttonClip);
-            }
-        }
 
         public void onMusicVolumeChange(Component sender, object data)
         {
@@ -76,7 +71,7 @@ namespace Supinfo.Project.Scripts.Managers
             audioSourceMusic.mute = isMuted;
             PlayerPrefs.SetInt("isMuted", isMuted ? 1 : 0);
         }
-        
+
         public void onSoundVolumeChange(Component sender, object data)
         {
             if (data is not float soundVolume) return;
@@ -89,6 +84,32 @@ namespace Supinfo.Project.Scripts.Managers
             if (data is not bool isSoundMuted) return;
             audioSourceSound.mute = isSoundMuted;
             PlayerPrefs.SetInt("isSoundMuted", isSoundMuted ? 1 : 0);
+        }
+
+        public void onPlayButtonClickSound(Component sender, object data)
+        {
+            if (data is AudioClip buttonClip && audioSourceSound != null)
+            {
+                audioSourceSound.PlayOneShot(buttonClip);
+            }
+        }
+        
+        public void ToggleMainMenuMusic(AudioClip easterEggMusic)
+        {
+            if (sceneName == SceneName.mainMenu && audioSourceMusic != null)
+            {
+                if (isEasterEggMusicPlaying)
+                {
+                    audioSourceMusic.clip = originalMainMenuMusic;
+                }
+                else
+                {
+                    audioSourceMusic.clip = easterEggMusic;
+                }
+
+                isEasterEggMusicPlaying = !isEasterEggMusicPlaying;
+                audioSourceMusic.Play();
+            }
         }
     }
 }
