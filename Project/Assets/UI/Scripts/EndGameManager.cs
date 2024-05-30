614f84ce3f8c38a9da9fb573775035550f882e85
Enzorbt <enzo.robert@supinfo.com>
2024-05-31 01:13:17 +0200

    :sparkles: Navigation (after ending game) to MainMenu or to restart the current game lv (getActiveScene) --> Instansciate but not complete

--- Project/Assets/UI/Scripts/EndGameManager.cs
+++ Project/Assets/UI/Scripts/EndGameManager.cs
@@ -1,23 +1,29 @@
-﻿
-
-using System;
+﻿using System;
+using Supinfo.Project.Scripts.Events;
 using Supinfo.Project.Scripts.Managers;
 using TMPro;
 using UnityEngine;
-using Image = UnityEngine.UI.Image;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Supinfo.Project.UI.Scripts
 {
     public class EndGameManager : MonoBehaviour
     {
         [SerializeField] private GameObject winPanel;
+        [SerializeField] private GameEvent onRestart;
+        [SerializeField] private GameEvent onReturnToMainMenu;
+
         private TextMeshProUGUI _text;
-        private Image _image;
+        private Image _bannerImage;
+        private Color _originalBannerColor;
 
         private void Awake()
         {
-            _text = winPanel.GetComponentInChildren<TextMeshProUGUI>();
-            _image = winPanel.GetComponentInChildren<Image>();
+            GameObject banner = winPanel.transform.Find("Banner").gameObject;
+            _text = banner.GetComponentInChildren<TextMeshProUGUI>();
+            _bannerImage = banner.GetComponentInChildren<Image>();
+            _originalBannerColor = _bannerImage.color;
         }
 
         public void OnCastleDeath(Component sender, object data)
@@ -27,7 +33,7 @@ namespace Supinfo.Project.UI.Scripts
             switch (baseId)
             {
                 case BaseIdentifier.BaseEnemies:
-                    EnableWinPanel(Color.white, "Victory");
+                    EnableWinPanel(_originalBannerColor, "Victory");
                     break;
                 case BaseIdentifier.BaseAllies:
                     EnableWinPanel(Color.red, "Defeat");
@@ -37,10 +43,20 @@ namespace Supinfo.Project.UI.Scripts
             }
         }
 
-        private void EnableWinPanel(Color color, String title)
+        private void EnableWinPanel(Color color, string title)
         {
             _text.text = title;
-            _image.color = color;
+            _bannerImage.color = color;
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void ReturnToMainMenu()
+        {
+            SceneManager.LoadScene("Main Menu");
         }
     }
 }
