a7dee4bf5acb8217f2d4580ec27d2d0b543f3ef4
Enzorbt <enzo.robert@supinfo.com>
2024-06-01 01:09:54 +0200

    :sparkles: Cooldown animation added for SpawnButtonUI.cs & SpecialCapacityButtonUI.cs

--- Project/Assets/UI/Button/Scripts/SpawnButtonUI.cs
+++ Project/Assets/UI/Button/Scripts/SpawnButtonUI.cs
@@ -1,19 +1,14 @@
-using System;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 using ScriptableObjects.Unit;
 using Supinfo.Project.Scripts.Events;
 using Supinfo.Project.Scripts.Managers;
-using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 namespace Supinfo.Project.UI.Button.Scripts
 {
     public class SpawnButtonUI : MonoBehaviour
     {
-        /// <summary>
-        /// The unit spawn So with the prefab and cooldown stat
-        /// </summary>
         [SerializeField]
         private UnitStatSo unitStatSo;
 
@@ -30,33 +25,31 @@ namespace Supinfo.Project.UI.Button.Scripts
 
         private Image _image;
 
-        /// <summary>
-        /// Event to be raised when the button is clicked.
-        /// </summary>
         [SerializeField] private GameEvent onSpawnUnit;
-        
-        [SerializeField]
-        private GameEvent onGoldChange;
+        [SerializeField] private GameEvent onGoldChange;
 
         private bool _canSpawn = true;
-        
         private UnityEngine.UI.Button _button;
-
         private float _goldCount;
-
         private bool _queueStatus = true;
+        
+        [SerializeField] private Image cooldownImage;
 
         private void Awake()
         {
             _button = transform.GetComponentInChildren<UnityEngine.UI.Button>();
             IsActive = true;
             _image = GetComponentsInChildren<Image>()[1];
+            
+            if (cooldownImage != null)
+            {
+                cooldownImage.type = Image.Type.Filled;
+                cooldownImage.fillMethod = Image.FillMethod.Radial360;
+                cooldownImage.fillOrigin = (int)Image.Origin360.Top;
+                cooldownImage.fillAmount = 0f;
+            }
         }
 
-        /// <summary>
-        /// Method to be called when the button is clicked.
-        /// Raises the onClick event with the associated unit.
-        /// </summary>
         public void OnClick()
         {
             if (_canSpawn && IsActive)
@@ -68,25 +61,35 @@ namespace Supinfo.Project.UI.Button.Scripts
         private IEnumerator SpawnWithCooldown()
         {
             _canSpawn = false;
-            
             EnableButton(_goldCount >= unitStatSo.Price && _queueStatus && _canSpawn);
-            
             onSpawnUnit.Raise(this, unitStatSo);
-            
-            onGoldChange.Raise(this, - unitStatSo.Price);
-            
+            onGoldChange.Raise(this, -unitStatSo.Price);
+
+            if (cooldownImage != null)
+            {
+                float elapsedTime = 0f;
+                float cooldownDuration = unitStatSo.BuildTime;
+
+                while (elapsedTime < cooldownDuration)
+                {
+                    elapsedTime += Time.deltaTime;
+                    cooldownImage.fillAmount = elapsedTime / cooldownDuration;
+                    yield return null;
+                }
+
+                cooldownImage.fillAmount = 0f;
+            }
+
             yield return new WaitForSeconds(unitStatSo.BuildTime);
-            
+
             _canSpawn = true;
-            
             EnableButton(_goldCount >= unitStatSo.Price && _queueStatus && _canSpawn);
         }
 
-        // function to change visual and enable the button
         private void EnableButton(bool value)
         {
-            if (_button is null) return;
-            if(!IsActive) return;
+            if (_button == null) return;
+            if (!IsActive) return;
             _button.enabled = value;
         }
 
@@ -99,16 +102,14 @@ namespace Supinfo.Project.UI.Button.Scripts
 
         public void OnGoldCountChange(Component sender, object data)
         {
-            if(data is not float goldCount) return;
+            if (data is not float goldCount) return;
             _goldCount = goldCount;
             EnableButton(_goldCount >= unitStatSo.Price && _queueStatus && _canSpawn);
         }
 
-
         public void OnGameSpeedChange(Component sender, object data)
         {
             if (data is not GameSpeed gameSpeed) return;
-            
             EnableButton(gameSpeed == GameSpeed.Stop ? false : _goldCount >= unitStatSo.Price && _queueStatus && _canSpawn);
         }
 
@@ -123,5 +124,4 @@ namespace Supinfo.Project.UI.Button.Scripts
             _image.sprite = unitStatSo.Sprite;
         }
     }
-
 }
