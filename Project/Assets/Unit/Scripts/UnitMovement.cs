4c2607f4f2880371373e07b753fc0b065e040422
romainboudou <118806640+romainboudou@users.noreply.github.com>
2024-05-31 02:12:13 +0200

    Animation
    All Unit Animations

--- Project/Assets/Unit/Scripts/UnitMovement.cs
+++ Project/Assets/Unit/Scripts/UnitMovement.cs
@@ -1,4 +1,5 @@
-﻿using Supinfo.Project.Scripts.Interfaces;
+﻿using System;
+using Supinfo.Project.Scripts.Interfaces;
 using UnityEngine;
 
 
@@ -6,9 +7,18 @@ namespace Supinfo.Project.Unit.Scripts
 {
     public class UnitMovement : MonoBehaviour, IMovement
     {
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
         public void Move(Vector3 direction, float speed)
         {
+            _animator.SetBool("walk", true);
             transform.Translate(speed * Time.deltaTime * direction);
+            //_animator.SetBool("walk", false);
         }
     }
 }
