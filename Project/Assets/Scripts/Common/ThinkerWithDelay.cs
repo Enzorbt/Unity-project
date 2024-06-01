4b1baa3de2e0a59c2c02f50d94fc57fcf931cf60
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-06-01 15:56:51 +0200

    New comment

--- Project/Assets/Scripts/Common/ThinkerWithDelay.cs
+++ Project/Assets/Scripts/Common/ThinkerWithDelay.cs
@@ -1,25 +1,50 @@
-﻿using Common;
+﻿using Supinfo.Project.Common;
 using UnityEngine;
 
-namespace Supinfo.Project.Common
+/// <summary>
+/// This class represents a thinker with a delay. It uses a BrainWithDelay object to perform thinking.
+/// </summary>
+public class ThinkerWithDelay : MonoBehaviour
 {
-    public class ThinkerWithDelay : MonoBehaviour
+    /// <summary>
+    /// A flag that indicates whether the thinker is currently thinking.
+    /// </summary>
+    public bool IsThinking {get; set;}
+
+    /// <summary>
+    /// The brain that the thinker uses to perform thinking.
+    /// </summary>
+    [SerializeField]
+    private BrainWithDelay brain;
+
+    /// <summary>
+    /// The brain that the thinker uses to perform thinking.
+    /// </summary>
+    public BrainWithDelay Brain
     {
-        public bool IsThinking {get; set;}
-        [SerializeField]
-        private BrainWithDelay brain;
+        get => brain;
+        set => brain = value;
+    }
 
-        public BrainWithDelay Brain
+    /// <summary>
+    /// This method is called by Unity every physics frame. It checks if the thinker is not currently thinking
+    /// and if the brain is not null, then it starts a coroutine to perform thinking with a delay.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Check if the thinker is not currently thinking
+        if (!IsThinking)
         {
-            get => brain;
-            set => brain = value;
-        }
-
-        private void FixedUpdate()
-        {
-            if (!IsThinking && brain is not null)
+            // Check if the brain is not null
+            if (brain != null)
             {
+                // Start a coroutine to perform thinking with a delay
                 StartCoroutine(brain.ThinkWithDelay(this));
+            }
+            else
+            {
+                // Log an error message if the brain is null
+                Debug.LogError("Brain is null in ThinkerWithDelay");
             }
         }
     }
