edd17cca8565723dab4eb9b5a55786eb10698d01
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-06-01 19:29:31 +0200

    NEW COMMENT

--- Project/Assets/Scripts/ScriptableObjects/Data/UnitData.cs
+++ Project/Assets/Scripts/ScriptableObjects/Data/UnitData.cs
@@ -5,86 +5,129 @@ using UnityEngine;
 namespace Supinfo.Project.Scripts.ScriptableObjects.Data
 {
     /// <summary>
-    ///  UnitData is a ScriptableObject used for storing data for various unit types in the game.
-    ///  It includes details about general properties, unit characteristics, and rewards for defeating the unit.
+    /// UnitData is a ScriptableObject used for storing data for various unit types in the game.
+    /// It includes details about general properties, unit characteristics, and rewards for defeating the unit.
     /// </summary>
     // [CreateAssetMenu(fileName = "UnitData", menuName = "ScriptableObject/Units/UnitData", order = 1)]
     public class UnitData : ScriptableObject
     {
         //--------- General properties ---------//
-        
+
         /// <summary>
         /// Array of GameObject prefabs for the unit. This can store sprite data or other prefab related data.
         /// </summary>
         [Header("General properties")]
         [SerializeField] private GameObject[] prefabs;
-        public GameObject[] Prefab => prefabs; // transferred
-        
+
+        /// <summary>
+        /// Gets the array of prefabs.
+        /// </summary>
+        public GameObject[] Prefab => prefabs;
+
         //--------- Characteristics ---------//
-        
+
         /// <summary>
         /// Type of the unit, defined in UnitType.
         /// </summary>
-        [Header("Characteristics")] 
+        [Header("Characteristics")]
         [SerializeField] private UnitType type;
-        public UnitType Type => type; 
-        
+
+        /// <summary>
+        /// Gets the type of the unit.
+        /// </summary>
+        public UnitType Type => type;
+
         /// <summary>
         /// Walking speed of the unit.
         /// </summary>
         [SerializeField] private Stat walkSpeed;
-        public Stat WalkSpeed => walkSpeed; // transferred
+
+        /// <summary>
+        /// Gets the walking speed of the unit.
+        /// </summary>
+        public Stat WalkSpeed => walkSpeed;
 
         /// <summary>
         /// Purchase price of the unit.
         /// </summary>
         [SerializeField] private Stat price;
-        public Stat Price => price; // transferred
-        
+
+        /// <summary>
+        /// Gets the purchase price of the unit.
+        /// </summary>
+        public Stat Price => price;
+
         /// <summary>
         /// Damage dealt by the unit.
         /// </summary>
         [SerializeField] private Stat damage;
+
+        /// <summary>
+        /// Gets the damage dealt by the unit.
+        /// </summary>
         public Stat Damage => damage;
-        
+
         /// <summary>
         /// Attack speed of the unit.
         /// </summary>
         [SerializeField] private Stat hitSpeed;
+
+        /// <summary>
+        /// Gets the attack speed of the unit.
+        /// </summary>
         public Stat HitSpeed => hitSpeed;
-        
+
         /// <summary>
         /// Time required to build or spawn the unit.
         /// </summary>
         [SerializeField] private Stat buildTime;
-        public Stat BuildTime => buildTime; // transferred
-        
+
+        /// <summary>
+        /// Gets the build time of the unit.
+        /// </summary>
+        public Stat BuildTime => buildTime;
+
         /// <summary>
         /// Attack range of the unit.
         /// </summary>
         [SerializeField] private Stat range;
+
+        /// <summary>
+        /// Gets the attack range of the unit.
+        /// </summary>
         public Stat Range => range;
-        
+
         /// <summary>
         /// Maximum health of the unit.
         /// </summary>
         [SerializeField] private Stat maxHealth;
-        public Stat MaxHealth => maxHealth; // transferred
+
+        /// <summary>
+        /// Gets the maximum health of the unit.
+        /// </summary>
+        public Stat MaxHealth => maxHealth;
 
         //--------- Rewards upon killing ---------//
-        
+
         /// <summary>
         /// Amount of gold given for defeating the unit.
         /// </summary>
         [Header("Rewards upon killing")]
         [SerializeField] private Stat goldGiven;
-        public Stat GoldGiven => goldGiven; // transferred
-        
+
+        /// <summary>
+        /// Gets the amount of gold given for defeating the unit.
+        /// </summary>
+        public Stat GoldGiven => goldGiven;
+
         /// <summary>
         /// Amount of experience given for defeating the unit.
         /// </summary>
         [SerializeField] private Stat experienceGiven;
-        public Stat ExperienceGiven => experienceGiven; // transferred
+
+        /// <summary>
+        /// Gets the amount of experience given for defeating the unit.
+        /// </summary>
+        public Stat ExperienceGiven => experienceGiven;
     }
 }
-
--- Project/Assets/Scripts/ScriptableObjects/Experience/ExperienceStatSo.cs
+++ Project/Assets/Scripts/ScriptableObjects/Experience/ExperienceStatSo.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 namespace Supinfo.Project.Scripts.ScriptableObjects.Experience
 {
+    /// <summary>
+    /// ExperienceStatSo is a ScriptableObject used for storing experience level data.
+    /// </summary>
     [CreateAssetMenu(menuName = "scriptableObject/stat", order = 0)]
     public class ExperienceStatSo : ScriptableObject
     {
+        /// <summary>
+        /// List of experience levels.
+        /// </summary>
         [SerializeField] private List<float> experienceLevels;
+
+        /// <summary>
+        /// Gets the list of experience levels.
+        /// </summary>
         public List<float> ExperienceLevel => experienceLevels;
     }
 }
