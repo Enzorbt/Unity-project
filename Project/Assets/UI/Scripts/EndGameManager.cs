ca88e6f668259071600ecba5c8cd85346814b021
Paul Preuvost <118807111+PaulPreuvost@users.noreply.github.com>
2024-06-01 20:09:40 +0200

    Finish les commentaires

--- Project/Assets/UI/Scripts/EndGameManager.cs
+++ Project/Assets/UI/Scripts/EndGameManager.cs
@@ -8,16 +8,45 @@ using UnityEngine.UI;
 
 namespace Supinfo.Project.UI.Scripts
 {
+    /// <summary>
+    /// This class manages the end game screen, displaying victory or defeat.
+    /// </summary>
     public class EndGameManager : MonoBehaviour
     {
+        /// <summary>
+        /// The GameObject representing the win/lose panel.
+        /// </summary>
         [SerializeField] private GameObject winPanel;
+
+        /// <summary>
+        /// The GameEvent that is triggered when the game is restarted.
+        /// </summary>
         [SerializeField] private GameEvent onRestart;
+
+        /// <summary>
+        /// The GameEvent that is triggered when the main menu is returned to.
+        /// </summary>
         [SerializeField] private GameEvent onReturnToMainMenu;
 
+        /// <summary>
+        /// The TextMeshProUGUI component that displays the victory/defeat text.
+        /// </summary>
         private TextMeshProUGUI _text;
+
+        /// <summary>
+        /// The Image component that displays the victory/defeat banner.
+        /// </summary>
         private Image _bannerImage;
+
+        /// <summary>
+        /// The original color of the victory/defeat banner.
+        /// </summary>
         private Color _originalBannerColor;
 
+        /// <summary>
+        /// This method is called when the script instance is being loaded.
+        /// It initializes the text and image components.
+        /// </summary>
         private void Awake()
         {
             GameObject banner = winPanel.transform.Find("Banner").gameObject;
@@ -26,6 +55,12 @@ namespace Supinfo.Project.UI.Scripts
             _originalBannerColor = _bannerImage.color;
         }
 
+        /// <summary>
+        /// This method is called when the castle is destroyed.
+        /// It displays the victory/defeat screen depending on the destroyed castle.
+        /// </summary>
+        /// <param name="sender">The component that sent the event.</param>
+        /// <param name="data">The data associated with the event.</param>
         public void OnCastleDeath(Component sender, object data)
         {
             if (data is not BaseIdentifier baseId) return;
@@ -43,17 +78,28 @@ namespace Supinfo.Project.UI.Scripts
             }
         }
 
+        /// <summary>
+        /// This method enables the victory/defeat screen with the specified color and title.
+        /// </summary>
+        /// <param name="color">The color of the victory/defeat banner.</param>
+        /// <param name="title">The text to display on the victory/defeat screen.</param>
         private void EnableWinPanel(Color color, string title)
         {
             _text.text = title;
             _bannerImage.color = color;
         }
 
+        /// <summary>
+        /// This method restarts the current scene.
+        /// </summary>
         public void RestartGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
+        /// <summary>
+        /// This method loads the main menu scene.
+        /// </summary>
         public void ReturnToMainMenu()
         {
             SceneManager.LoadScene("Main Menu");
--- Project/Assets/UI/Scripts/GoldTextUi.cs
+++ Project/Assets/UI/Scripts/GoldTextUi.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 
 namespace Supinfo.Project.UI.Scripts
 {
+    /// <summary>
+    /// This class manages the display of the player's gold in the UI.
+    /// </summary>
     public class GoldTextUi : MonoBehaviour
     {
-        
-        [SerializeField]
-        private GameEvent onGoldChange;
-        
+        /// <summary>
+        /// The GameEvent that is triggered when the player's gold changes.
+        /// </summary>
+        [SerializeField] private GameEvent onGoldChange;
+
+        /// <summary>
+        /// This method is called when the player's gold changes.
+        /// It updates the text displaying the player's gold.
+        /// </summary>
+        /// <param name="sender">The component that sent the event.</param>
+        /// <param name="data">The data associated with the event.</param>
         public void OnGoldChange(Component sender, object data)
         {
-            
+            // If the data is not a float, return
             if (data is not float gold) return;
+
+            // Try to get the TextMeshProUGUI component from the transform
             transform.TryGetComponent(out TextMeshProUGUI textMeshPro);
+
+            // If the TextMeshProUGUI component is null, return
             if (textMeshPro is null) return;
+
+            // Update the text displaying the player's gold
             textMeshPro.text = gold.ToString(CultureInfo.CurrentCulture);
         }
     }
