85cb898a8d75f4b8655e99c88402e52cc2ea5d01
Enzorbt <enzo.robert@supinfo.com>
2024-06-02 02:24:03 +0200

    :sparkles: LAST COMMIT FROM ENZO (god damn..)

---
+++ Project/Assets/UI/Scripts/WinPanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Supinfo.Project.UI.Scripts
+{
+    public class WinPanel : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
+}
